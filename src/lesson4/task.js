30347a800b62b8cf69f5629b2386d778e9c3225d
R.Rzhenetskyy <rostyslav.rzhenetskyy@gmail.com>
2018-02-06 17:17:39 +0200

    added: createMap from Task 4

--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -57,8 +57,67 @@ export function createSet(arr = []) {
   in forEach() method.
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
-export function createMap(arr) {
-
+export function createMap(arr = []) {
+  const mapPrototype = {
+    get size() {
+      return this.length || 0;
+    },
+    has(key) {
+      return Array.prototype.filter.call(this, (element => element[0] === key)).length > 0;
+    },
+    set(...rest) {
+      if (rest) {
+        try {
+          Array.prototype.filter.call(this, (elem => elem[0] === rest[0]))[0][1] = rest[1];
+        } catch(e) {
+          Array.prototype.push.call(this, [rest[0], rest[1]]);
+        }
+      } else {
+        try {
+          Array.prototype.filter.call(this, (elem => elem[0] === undefined))[0][1] = undefined;
+        } catch(e) {
+          if (e instanceof ReferenceError) {
+            Array.prototype.push.call(this, [undefined, undefined]);
+          }
+        }
+      }
+    },
+    delete(val) {
+      if (val) {
+        try {
+          Array.prototype.filter.call(this, elem => {
+            if (elem[0] === val) {
+              Array.prototype.splice.call(this, (Array.prototype.indexOf.call(this, elem)), 1);
+              return true;
+            }
+          })
+        } catch(e) {
+          return false;
+        }
+      }
+      return false;
+    },
+    clear() {
+      Array.prototype.splice.call(this, 0);
+    },
+    forEach(fn) {
+      return Array.prototype.forEach.call(this, element => fn(element[1], element[0]));
+    }
+  };
+  const arrMap = [];
+  if (arr && Array.isArray(arr)) {
+    arr.forEach(arrElement => {
+      try {
+        arrMap.filter(elem => elem[0] === arrElement[0])[0][1] = arrElement[1];
+      } catch (e) {
+        if (e instanceof TypeError) {
+          arrMap.push(arrElement);
+        }
+      }
+    });
+  }
+  Object.setPrototypeOf(arrMap, mapPrototype);
+  return arrMap;
 }
 
 export default {
