8c1c1d2768d1a574e7a1b1c8429f08e1d3a2eea3
R.Rzhenetskyy <rostyslav.rzhenetskyy@gmail.com>
2018-02-13 16:46:34 +0200

    Complete task6

--- src/lesson6/task.js
+++ src/lesson6/task.js
@@ -2,26 +2,48 @@
 
 // Климат на любой планете по её имени
 // {planetName} – String
-const getClimate = function(planetName) {
-
+function getClimate(planetName) {
+  const baseUrl = 'https://swapi.co/api/planets/?search=';
+  return fetch(`${baseUrl}${planetName}`)
+    .then(res => res.json())
+    .then(response => response.results[0].climate)
+    .catch(error => { throw new Error(error); });
 }
 
 // Получить информацию (Object) о любом персонаже по имени
 // {name} – String
-const getProfile = function(name) {
-
+function getProfile(name) {
+  const baseUrl = 'https://swapi.co/api/people/?search=';
+  return fetch(`${baseUrl}${name}`)
+    .then(res => res.json())
+    .then(response => response.results[0])
+    .catch(error => { throw new Error(error); });
 }
 
 // Получить список пилотов (имена в виде Array of Strings) космического корабля
 // по его названию
 // {starshipName} - String
-const getPilots = function(starshipName) {
-
+async function getPilots(shipName) {
+  const baseUrl = 'https://swapi.co/api/starships/?search=';
+  const nameLinks = [];
+  try {
+    const starShip = await fetch(`${baseUrl}${shipName}`)
+      .then(res => res.json())
+      .then(response => response.results[0]);
+    await starShip.pilots.forEach(link => nameLinks.push(link));
+  } catch (ex) {
+    throw new Error(ex);
+  }
+  const promises = nameLinks.map(link => (
+    fetch(link)
+      .then(res => res.json())
+      .then(response => response.name)
+  ));
+  return Promise.all(promises);
 }
-
 
 export default {
   getClimate,
   getProfile,
-  getPilots
-}
+  getPilots,
+};
